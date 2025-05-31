a2c89d710a0929bde7f41a46855da43729781009
Krzysztof <krzyspanfilewicz@gmail.com>
2025-05-31 23:53:44 +0200

    added validation

--- MvcMovie/Models/SeedData.cs
+++ MvcMovie/Models/SeedData.cs
@@ -26,6 +26,7 @@ namespace MvcMovie.Models
                         Title = "When Harry Met Sally",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
+                        Rating = "PG-13",
                         Price = 7.99M
                     },
 
@@ -34,6 +35,7 @@ namespace MvcMovie.Models
                         Title = "Ghostbusters ",
                         ReleaseDate = DateTime.Parse("1984-3-13"),
                         Genre = "Comedy",
+                        Rating = "PG-13",
                         Price = 8.99M
                     },
 
@@ -42,7 +44,9 @@ namespace MvcMovie.Models
                         Title = "Ghostbusters 2",
                         ReleaseDate = DateTime.Parse("1986-2-23"),
                         Genre = "Comedy",
+                        Rating = "PG-16",
                         Price = 9.99M
+
                     },
 
                     new Movie
@@ -50,6 +54,7 @@ namespace MvcMovie.Models
                         Title = "Rio Bravo",
                         ReleaseDate = DateTime.Parse("1959-4-15"),
                         Genre = "Western",
+                        Rating = "R",
                         Price = 3.99M
                     }
                 );
